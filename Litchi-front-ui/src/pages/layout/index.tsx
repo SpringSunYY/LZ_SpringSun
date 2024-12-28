3fa49ca87027f62cfceda76eff7b5fca275f2918
SpringSunYY <3217778348@qq.com>
2024-12-28 16:11:23 +0800

    搜索弹出层

--- Litchi-front-ui/src/pages/layout/index.tsx
+++ Litchi-front-ui/src/pages/layout/index.tsx
@@ -1,5 +1,5 @@
-import React, {Fragment, useState} from "react";
-import {AppBar, Button, Drawer, Grid, Menu, MenuItem, useMediaQuery} from "@mui/material";
+import React, {useState} from "react";
+import {AppBar, Box, Button, Drawer, Grid, Menu, MenuItem, Modal, useMediaQuery} from "@mui/material";
 import {Outlet, useNavigate} from "react-router-dom";
 import {useTheme} from "@mui/system";
 import lzIcon from "@/assets/icons/svg/lz.svg"
@@ -39,7 +39,18 @@ const menus = [
         // ],
     }
 ];
-
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    // border: '2px solid #000',
+    // boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
 const GeekLayout: React.FC = () => {
     const navigate = useNavigate();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null); // 当前菜单的锚点
@@ -47,6 +58,14 @@ const GeekLayout: React.FC = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm")); // 检测屏幕尺寸
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpenModal = () => {
+        setOpen(true);
+    };
+    const handleCloseModal = () => {
+        setOpen(false);
+    };
 
     // 处理点击按钮显示菜单
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>, menu: object) => {
@@ -82,7 +101,7 @@ const GeekLayout: React.FC = () => {
             <header className="header">
                 <AppBar className="appBar" position="sticky">
                     <Grid container className={"toolBar"}>
-                        <Grid item xs={2} className="title"><img src={lzIcon} width="80em" height="80em" alt="lz icon"/>
+                        <Grid item xs={2} className="title-log"><img src={lzIcon} width="80em" height="80em" alt="lz icon"/>
                         </Grid>
                         <Grid item xs={10} className="menu">
                             {/* 当是小屏幕时，点击按钮展开折叠菜单 */}
@@ -90,10 +109,10 @@ const GeekLayout: React.FC = () => {
                                 <Button onClick={toggleDrawer}> <span className={"menu-content menu-content-hover"}
                                                                       style={{marginLeft: "8em"}}>菜单</span></Button>
                             ) : (
-                                <Grid container>
+                                <Grid container columns={12}>
                                     <Grid xs={8} className={"menu-item"}> {menus.map((menu) => (
                                         <span key={menu.menuId}>
-                                            {/* 菜单按钮 */}
+                                                {/* 菜单按钮 */}
                                             <Button
                                                 onClick={(event) => handleClick(event, menu)} // 点击时设置锚点并展开菜单
                                                 aria-controls={`menu-${menu.menuId}`}
@@ -103,10 +122,10 @@ const GeekLayout: React.FC = () => {
                                                     paddingX: '0em',   // 可以调整左右内边距
                                                 }}
                                             >
-                                            <span className={"menu-content menu-content-hover"}>
-                                              {menu.label}
-                                             </span>
-                                            </Button>
+                                                <span className={"menu-content menu-content-hover"}>
+                                                  {menu.label}
+                                                 </span>
+                                                </Button>
                                             {/* 大屏幕下的菜单项 */}
                                             <Menu
                                                 anchorEl={anchorEl} // 当前按钮作为菜单的锚点
@@ -116,34 +135,56 @@ const GeekLayout: React.FC = () => {
                                                     "aria-labelledby": `menu-${menu.menuId}`,
                                                 }}
                                             >
-                                            {menu?.children && menu.children.length > 0 && (
-                                                menu.children.map((child) => (
-                                                    <MenuItem key={child.menuId}
-                                                              onClick={() => handleMenuItemClick(child)}>
-                                                        {child.label}
-                                                    </MenuItem>
-                                                ))
-                                            )}
-                                    </Menu>
-                                </span>
-                                    ))}</Grid>
-                                    <Grid item xs={4} spacing={2} className={"menu-right menu-content"}>
-                                        <Grid container>
-                                            <Grid item xs={2} className={"menu-content-hover"}> <MySvgIcon name={"qq"}
-                                                                                                           size={"1.5em"}/>
+                                                {menu?.children && menu.children.length > 0 && (
+                                                    menu.children.map((child) => (
+                                                        <MenuItem key={child.menuId}
+                                                                  onClick={() => handleMenuItemClick(child)}>
+                                                            {child.label}
+                                                        </MenuItem>
+                                                    ))
+                                                )}
+                                        </Menu>
+                                    </span>
+                                    ))}
+                                    </Grid>
+                                    <Grid item xs={4} className={"menu-right menu-content"}>
+                                        <Grid container spacing={1} columns={16}>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon name={"qq"} size={"1.5em"}/>
                                             </Grid>
-                                            <Grid item xs={2} className={"menu-content-hover"}><MySvgIcon name={"wx"}
-                                                                                                          size={"1.5em"}/></Grid>
-                                            <Grid item xs={2} className={"menu-content-hover"}><MySvgIcon
-                                                name={"facebook"} size={"1.5em"}/></Grid>
-                                            <Grid item xs={2} className={"menu-content-hover"}><MySvgIcon
-                                                name={"twitter"} size={"1.5em"}/></Grid>
-                                            <Grid item xs={2}
-                                                  className={"menu-content-hover"}><MySvgIcon
-                                                name={"lz"} size={"1.5em"}/></Grid>
-                                            <Grid item xs={2}
-                                                  className={"menu-content-hover"}><MySvgIcon
-                                                name={"search"} size={"1.5em"}/></Grid>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon name={"wx"} size={"1.5em"}/>
+                                            </Grid>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon name={"facebook"} size={"1.5em"}/>
+                                            </Grid>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon name={"twitter"} size={"1.5em"}/>
+                                            </Grid>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon name={"lz"} size={"1.5em"}/>
+                                            </Grid>
+                                            <Grid item xs={2} className={"menu-content-hover"}>
+                                                <MySvgIcon onClick={handleOpenModal} name={"search"} size={"1.5em"}/>
+                                                <Modal
+                                                    open={open}
+                                                    onClose={handleCloseModal}
+                                                    aria-labelledby="parent-modal-title"
+                                                    aria-describedby="parent-modal-description"
+                                                >
+                                                    <Box sx={{...style, width: '80%'}}>
+                                                        <h2 id="parent-modal-title">Text in a modal</h2>
+                                                        <p id="parent-modal-description">
+                                                            Duis mollis, est non commodo luctus, nisi erat porttitor
+                                                            ligula.
+                                                        </p>
+                                                        {/*<ChildModal/>*/}
+                                                    </Box>
+                                                </Modal>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <Button variant="outlined">登录</Button>
+                                            </Grid>
                                         </Grid>
                                     </Grid>
                                 </Grid>
@@ -179,3 +220,4 @@ const GeekLayout: React.FC = () => {
 };
 
 export default GeekLayout;
+
