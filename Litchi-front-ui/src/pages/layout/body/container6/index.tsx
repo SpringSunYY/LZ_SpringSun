9f5ca0f1f11bab368c37534c585474363f889bfb
SpringSunYY <3217778348@qq.com>
2025-01-03 22:43:44 +0800

    container6

--- Litchi-front-ui/src/pages/layout/body/container6/index.tsx
+++ Litchi-front-ui/src/pages/layout/body/container6/index.tsx
@@ -1,7 +1,6 @@
 import "./index.scss"
 import {useTranslation} from "react-i18next";
 import {Container, Grid} from "@mui/material";
-import SlideIconButton from "@/compoents/SlideIconButton";
 import ImageWithZoom from "@/compoents/ImageWithZoom";
 
 interface Container6Props {
@@ -12,29 +11,65 @@ const Container6: React.FC<Container6Props> = ({isMediumScreen = false}: { isMed
     const {t} = useTranslation();
     console.log(t);
     return (
-        <div className={"container5"}>
+        <div className={"container6"}>
             {!isMediumScreen ? (
                 <Container>
-                    <ImageWithZoom src={"/src/assets/images/location/location_1.jpg"} label={"广州"} width={600}
-                                   height={400} labelFontSize={"60px"}></ImageWithZoom>
+                    <Grid container spacing={2} className={"container"}>
+                        <Grid item xs={12} className={"content"}>
+                            <Container>
+                                <h1 className={"title"}>
+                                    {t('Location Find Start Title')}
+                                </h1>
+                                <p className={"detail"}>
+                                    {t('Location Find Start Detail')}
+                                </p>
+                            </Container>
+                        </Grid>
+                        <Grid item xs={6} className={"images"}>
+                            <ImageWithZoom src={"/src/assets/images/location/location_1.jpg"} label={"GuangZhou"}
+                                           width={"100%"} height={'313px'} labelFontSize={40}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Grid container spacing={1} columns={10}>
+                                <Grid item xs={5}>
+                                    <ImageWithZoom src={"/src/assets/images/location/location_2.jpg"}
+                                                   label={"GuangZhou"}
+                                                   width={"280px"} height={'150px'}/>
+                                </Grid>
+                                <Grid item xs={5}>
+                                    <ImageWithZoom src={"/src/assets/images/location/location_3.jpg"} label={"Europe"}
+                                                   width={"280px"} height={'150px'}/>
+                                </Grid>
+                                <Grid item xs={5}>
+                                    <ImageWithZoom src={"/src/assets/images/location/location_4.jpg"} label={"BeiJing"}
+                                                   width={"280px"} height={'150px'}/>
+                                </Grid>
+                                <Grid item xs={5}>
+                                    <ImageWithZoom src={"/src/assets/images/location/location_1.jpg"} label={"ShangHai"}
+                                                   width={"280px"} height={'150px'}/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
                 </Container>
             ) : (
                 <Container>
-                    <Grid container className={"container"}>
-                        <Grid item xs={12} className={"left"}>
-                            <h1 className={"title"}>Cirkle Makes Your Life Easier & Simple</h1>
-                            <p className={"content"}>Aliquam lorem ante dapibus in viverra quis feugiat atellu Peaselus
-                                vierra nullaut metus
-                                varius laoreet unknown printer took scrambled make.Aliquam lorem ante dapibus in viverra
-                                quis feugiat atellu Peaselus vierra nullaut metus varius laoreet unknown printer took
-                                scrambled make.</p>
-                            <p className={"icon-button"}>
-                                <SlideIconButton label={"Read more"}/>
-                            </p>
+                    <Grid container spacing={2} className={"container"}>
+                        <Grid item xs={12} className={"content"}>
+                            <Container>
+                                <h1 className={"title"}>
+                                    {t('Location Find Start Title')}
+                                </h1>
+                                <p className={"detail"}>
+                                    {t('Location Find Start Detail')}
+                                </p>
+                            </Container>
                         </Grid>
                     </Grid>
                 </Container>
-            )}
+            )
+            }
         </div>
     )
 }
