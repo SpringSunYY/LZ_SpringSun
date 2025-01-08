683f9c22a22dbc72f598e73e7870f89a52b62e1e
SpringSunYY <3217778348@qq.com>
2025-01-08 20:24:41 +0800

    BannerAppsStart

--- Litchi-front-ui/src/pages/layout/body/bannerAppsStart/index.tsx
+++ Litchi-front-ui/src/pages/layout/body/bannerAppsStart/index.tsx
@@ -15,20 +15,20 @@ const BannerAppsStart: React.FC<BannerAppsStartProps> = ({isMediumScreen = false
                 <Container>
                     <Grid container className={"container"}>
                         <Grid item xs={6} className={"left"}>
-                            <h1 className={"title"}>{t('why choose title')}</h1>
-                            <p className={"content"}>{t('why choose detail')}</p>
+                            <h1 className={"title"}>{t('banner apps start title')}</h1>
+                            <p className={"content"}>{t('banner apps start detail')}</p>
                             <p className={"icon-button"}>
-                                <SlideIconButton label={t('why choose button')}/>
+                                <SlideIconButton label={t('banner apps start button')}/>
                             </p>
+                        </Grid>
+                        <Grid className={"right"} item xs={6}>
+                            <div className={"right-img"}>
+                                <img src="/src/assets/images/banner/apps.png" width={"55%"} height={"auto"}
+                                     alt={"why_choose_1.png"}/>
+                            </div>
                             <li>
                                 <img src={"/src/assets/images/figure/multi_shape.png"} alt={"multi_shape"}/>
                             </li>
-                        </Grid>
-                        <Grid className={"right"} item xs={6}>
-                            <div>
-                                <img src="/src/assets/images/figure/why_choose_1.jpg" width={"100%"} height={"auto"}
-                                     alt={"why_choose_1.png"}/>
-                            </div>
                         </Grid>
                     </Grid>
                 </Container>
@@ -36,10 +36,10 @@ const BannerAppsStart: React.FC<BannerAppsStartProps> = ({isMediumScreen = false
                 <Container>
                     <Grid container className={"container"}>
                         <Grid item xs={12} className={"left"}>
-                            <h1 className={"title"}>{t('why choose title')}</h1>
-                            <p className={"content"}>{t('why choose detail')}</p>
+                            <h1 className={"title"}>{t('banner apps start title')}</h1>
+                            <p className={"content"}>{t('banner apps start detail')}</p>
                             <p className={"icon-button"}>
-                                <SlideIconButton label={t('why choose button')}/>
+                                <SlideIconButton label={t('banner apps start button')}/>
                             </p>
                         </Grid>
                     </Grid>
